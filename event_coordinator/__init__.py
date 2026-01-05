cef12200bcef793fbe98492b792d91ac14650a6f
devkartikeyiitm <24f1000060@ds.study.iitm.ac.in>
2026-01-05 23:40:42 +0530

    Commit 1: Add package initialization with metadata

---
+++ event_coordinator/__init__.py
@@ -0,0 +1,9 @@
+"""Event Coordinator CLI - A tool for managing event coordination tasks."""
+
+__version__ = "0.1.0"
+__author__ = "devkartikey"
+__license__ = "MIT"
+
+from .cli import main
+
+__all__ = ["main"]
